98a3168f2b24c8e80dd0687e39bb9bff32938115
Dwi Sasongko S <ruckuus@gmail.com>
2014-06-01 15:29:04 +0000

    OCD

--- printing/cash_drawer.c
+++ printing/cash_drawer.c
@@ -3,11 +3,11 @@
 
 int open_cash_drawer(int fd)
 {
-	char pulse[5] = {0x1b, 0x70, 0x00, 0x25, 0x25};
+    char pulse[5] = {0x1b, 0x70, 0x00, 0x25, 0x25};
 
-	if (fd < 0) {
-    return -1;
-	}
+    if (fd < 0) {
+        return -1;
+    }
 
-	return write(fd, pulse, 5);
+    return write(fd, pulse, 5);
 }
